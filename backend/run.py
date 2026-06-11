eb40fe88006df40f4989f41bdd97b13ae0faa758
AutomationAuditsEnergies <automation.auditsenergies@gmail.com>
2026-06-11 15:51:11 +0200

    Run socrate1 with socketio server

--- backend/run.py
+++ backend/run.py
@@ -4,6 +4,8 @@ import eventlet
 eventlet.monkey_patch()
 
 # Maintenant on peut importer le reste
+import os
+
 from dotenv import load_dotenv
 
 load_dotenv()  # Charge le fichier .env
@@ -12,7 +14,7 @@ from main_app import app, socketio
 
 if __name__ == "__main__":
     host = "0.0.0.0"
-    port = 5001
+    port = int(os.getenv("PORT") or os.getenv("WEBSITES_PORT") or 5001)
     print(
         f"🚀 Serveur lance sur http://127.0.0.1:{port} (localhost) et http://{host}:{port} (sur le reseau)"
     )
