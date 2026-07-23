c2b056c74ea7aec3fe94dd570db1cd15c439ed9e
AutomationAuditsEnergies <automation.auditsenergies@gmail.com>
2026-07-23 13:15:07 +0200

    Place le néon autour des lettres

--- frontend/src/limovaClone/sections/HeroSection/index.tsx
+++ frontend/src/limovaClone/sections/HeroSection/index.tsx
@@ -40,7 +40,10 @@ export const HeroSection = () => {
               >
                 <span className="box-border caret-transparent text-[32.6923px] tracking-[-1.14423px] leading-[35.9615px] outline-[3px] no-underline md:text-[60.4444px] md:tracking-[-2.11556px] md:leading-[66.4889px]">
                   Des{" "}
-                  <span className="cadrenza-hero-neon-title">
+                  <span
+                    className="cadrenza-hero-neon-title"
+                    data-text="professeurs IA"
+                  >
                     professeurs IA
                   </span>
                 </span>
